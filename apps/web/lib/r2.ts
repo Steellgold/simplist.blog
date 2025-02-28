392722d105e6040a40316c1e50f387decf548bb9
Gaëtan H <me@gaetanhus.fr>
2025-02-28 09:30:19 +0100

    chore(r2): remove unnecessary comment from r2.ts

--- apps/web/lib/r2.ts
+++ apps/web/lib/r2.ts
@@ -1,4 +1,3 @@
-// lib/r2.ts
 import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
