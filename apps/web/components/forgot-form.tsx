477b8a4539cd9ed56f5abb2cb2af54792a375055
Gaëtan H <me@gaetanhus.fr>
2025-02-28 16:01:24 +0100

    feat(forgot-password): implement password reset form with validation and loading state

--- apps/web/components/forgot-form.tsx
+++ apps/web/components/forgot-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type React from "react"
 import { Component } from "@workspace/ui/components/utils/component"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@workspace/ui/components/card"
@@ -5,11 +7,23 @@ import { cn } from "@workspace/ui/lib/utils"
 import { Label } from "@workspace/ui/components/label"
 import { Input } from "@workspace/ui/components/input"
 import { Button } from "@workspace/ui/components/button"
+import { Alert, AlertDescription } from "@workspace/ui/components/alert"
+import { toast } from "@workspace/ui/hooks/use-toast"
+import { authClient } from "@/lib/auth-client"
+import { z } from "zod"
+import { useState } from "react";
+import { Loader2, MailCheck } from "lucide-react";
+
+const forgotPasswordSchema = z.object({
+  email: z.string().email(),
+});
 
 export const ForgotPasswordForm: Component<React.ComponentPropsWithoutRef<"div">> = ({
   className,
   ...props
 }) => {
+  const [isPending, setIsPending] = useState<boolean | "yet">(false);
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -18,15 +32,74 @@ export const ForgotPasswordForm: Component<React.ComponentPropsWithoutRef<"div">
           <CardDescription>Enter your email address to reset your password</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          {isPending === "yet" && (
+            <Alert>
+              <MailCheck className="w-6 h-6" />
+              <AlertDescription>
+                If an account with that email exists, we will send a password reset link to your email.
+              </AlertDescription>
+            </Alert>
+          )}
+
+          <form onSubmit={async(e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            if (isPending) return;
+            
+            const formData = Object.fromEntries(new FormData(e.currentTarget).entries());
+            const result = forgotPasswordSchema.safeParse(formData);
+            
+            if (result.error) {
+              toast({
+                title: "Data validation error",
+                description: "Please check the form fields and try again",
+                variant: "destructive",
+              })
+              return;
+            }
+
+            await authClient.forgetPassword({
+              email: result.data.email,
+              // redirectTo: "/auth/reset-password",
+              // TODO: Create a reset password page
+              redirectTo: "/auth",
+              fetchOptions: {
+                onRequest: () => {
+                  toast({
+                    title: "Sending reset password email",
+                    description: "Please check your email for instructions"
+                  });
+                  setIsPending(true);
+                },
+                onError: (error) => {
+                  toast({
+                    title: "Error sending reset password email",
+                    description: error.error.message || "Error sending reset password email",
+                    variant: "destructive"
+                  })
+
+                  setIsPending(false);
+                },
+                onSuccess: () => {
+                  toast({
+                    title: "Reset password email sent",
+                    description: "Please check your email for instructions"
+                  });
+
+                  setIsPending("yet");
+                }
+              }
+            })
+          }}>
             <div className="grid gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
+                <Input id="email" name="email" type="email" placeholder="m@example.com" required />
               </div>
+
               <Button type="submit" className="w-full">
-                Reset Password
+                {isPending !== false ? <Loader2 className="w-6 h-6 animate-spin" /> : "Reset Password"}
               </Button>
+
               <div className="text-center text-sm">
                 Remember your password?{" "}
                 <a href="/auth" className="underline underline-offset-4">
