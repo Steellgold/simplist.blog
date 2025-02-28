68600673645f8613c372a8a60e4d26b9286e7498
Gaëtan H <me@gaetanhus.fr>
2025-02-28 08:39:32 +0100

    feat(organization): enhance NoOrganizations component with validation and refactor NewOrganization to use it

--- apps/web/app/(app)/_components/no-organizations.tsx
+++ apps/web/app/(app)/_components/no-organizations.tsx
@@ -5,12 +5,30 @@ import { Button } from "@workspace/ui/components/button";
 import { Input } from "@workspace/ui/components/input";
 import { RadioPlanSelector } from "@workspace/ui/components/radio-plan-selector";
 import { BreadcrumbSetter } from "@workspace/ui/components/setter-breadcrumb";
+import { Component } from "@workspace/ui/components/utils/component";
 import { toast } from "@workspace/ui/hooks/use-toast";
+import { cn } from "@workspace/ui/lib/utils";
 import { Building, Loader2 } from "lucide-react"
 import { useRouter } from "next/navigation";
 import { ReactElement, useState } from "react";
+import { z } from "zod";
 
-export const NoOrganizations = (): ReactElement => {
+type NewOrganizationProps = {
+  isFrame?: boolean;
+};
+
+const schema = z.object({
+  organizationName: z.string()
+    .min(3, "Organization name must be at least 3 characters long.")
+    .max(50, "Organization name must be at most 50 characters long.")
+    .regex(/^[a-zA-Z0-9\s]+$/, "Organization name must only contain letters, numbers, and spaces.")
+    .nonempty("Organization name is required.")
+    .refine(value => value.trim() !== "", {
+      message: "Organization name cannot be empty.",
+    })
+});
+
+export const NoOrganizations: Component<NewOrganizationProps> = ({ isFrame }) => {
   const [plan, setPlan] = useState<"hobby" | "pro" | "business">("hobby");
   const [isPending, setPending] = useState(false);
   const router = useRouter();
@@ -20,16 +38,22 @@ export const NoOrganizations = (): ReactElement => {
       <BreadcrumbSetter items={[{ label: "Home", href: "/" }, { label: "Create your organization" }]} />
 
       <div className="flex justify-center">
-        <div className="w-full max-w-md space-y-8 md:p-8">
-          <div className="flex flex-col items-center gap-2">
-            <div className="flex size-16 shrink-0 items-center justify-center rounded-full border border-border" aria-hidden="true">
-              <Building className="h-8 w-8 text-primary" />
+        <div className={cn(
+          "w-full max-w-md space-y-8", {
+            "md:p-8": !isFrame,
+          }
+        )}>
+          {!isFrame && (
+            <div className="flex flex-col items-center gap-2">
+              <div className="flex size-16 shrink-0 items-center justify-center rounded-full border border-border" aria-hidden="true">
+                <Building className="h-8 w-8 text-primary" />
+              </div>
+              <h1 className="text-1xl md:text-2xl font-semibold tracking-tight">Create your organization</h1>
+              <p className="text-sm text-muted-foreground text-center">
+                You don&apos;t have an organization yet. Ready to create one?
+              </p>
             </div>
-            <h1 className="text-1xl md:text-2xl font-semibold tracking-tight">Create your organization</h1>
-            <p className="text-sm text-muted-foreground text-center">
-              You don&apos;t have an organization yet. Ready to create one?
-            </p>
-          </div>
+          )}
 
           <form className="space-y-6" onSubmit={async (event) => {
             if (isPending) return;
@@ -39,10 +63,21 @@ export const NoOrganizations = (): ReactElement => {
             event.preventDefault();
 
             const organizationName = event.currentTarget["organization-name"].value;
-            const organizationSlug = organizationName.toLowerCase().replace(/\s/g, "-") + "-" + randomChars();
+
+            const result = schema.safeParse({ organizationName });
+            if (!result.success) {
+              toast({
+                title: "Invalid organization name",
+                description: result.error.errors[0]?.message || "An error occurred while creating the organization.",
+                variant: "destructive"
+              })
+              return;
+            }
+
+            const organizationSlug = `${result.data.organizationName.toLowerCase().replace(/\s/g, "-")}-${randomChars()}`;
 
             await authClient.organization.create({
-              name: organizationName,
+              name: result.data.organizationName,
               slug: organizationSlug,
               metadata: {
                 plan
