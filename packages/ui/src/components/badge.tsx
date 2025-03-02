18e2f60a0f720964d5a329ff6a75370ebd76b19d
Gaëtan H <gaetanhus@gmail.com>
2025-03-02 15:53:33 +0100

    feat(badge): modify badge styles to use a rounded border instead of rounded-full

--- packages/ui/src/components/badge.tsx
+++ packages/ui/src/components/badge.tsx
@@ -6,7 +6,8 @@ import { cn } from "@workspace/ui/lib/utils";
 export type BadgeSubscriptionVariant = "subscriptionActive" | "subscriptionCanceled" | "subscriptionIncomplete" | "subscriptionIncompleteExpired" | "subscriptionPastDue" | "subscriptionTrialing" | "subscriptionUnpaid" | "subscriptionUnknown";
 
 const badgeVariants = cva(
-  "inline-flex items-center justify-center rounded-full border px-1.5 text-xs font-medium leading-normal transition-colors outline-offset-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70",
+  // rounded-full
+  "inline-flex items-center justify-center rounded border px-1.5 text-xs font-medium leading-normal transition-colors outline-offset-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70",
   {
     variants: {
       variant: {
