078059fa2fecb2d1117a950df3dc5d0c972125b9
Gaëtan H <gaetanhus@gmail.com>
2025-03-02 13:57:24 +0100

    feat(polar): add getStatus function to handle subscription status mapping

--- apps/web/lib/polar.ts
+++ apps/web/lib/polar.ts
@@ -1,6 +1,28 @@
 import { Polar } from "@polar-sh/sdk";
+import { SubscriptionStatus } from "@polar-sh/sdk/models/components/subscriptionstatus.js";
 
 export const polar = new Polar({
   server: process.env.POLAR_SERVER! as "production" | "sandbox",
   accessToken: process.env.POLAR_ACCESS_TOKEN!
 })
+
+export const getStatus = (status: SubscriptionStatus) => {
+  switch (status) {
+    case SubscriptionStatus.Active:
+      return "Active";
+    case SubscriptionStatus.Canceled:
+      return "Canceled";
+    case SubscriptionStatus.Incomplete:
+      return "Incomplete";
+    case SubscriptionStatus.IncompleteExpired:
+      return "Incomplete Expired";
+    case SubscriptionStatus.PastDue:
+      return "Past Due";
+    case SubscriptionStatus.Trialing:
+      return "Trial Period";
+    case SubscriptionStatus.Unpaid:
+      return "Unpaid";
+    default:
+      return "Unknown";
+  }
+}
