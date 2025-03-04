a0327a08f9d8cb8b6ecae3b8562ecab543012746
Gaëtan H <me@gaetanhus.fr>
2025-03-04 14:25:14 +0100

    feat(billing): enhance BillingCurrentPlanCard with subscription status and cancel dialog

--- apps/web/app/(app)/(organization)/settings/billing/_components/current-plan.card.tsx
+++ apps/web/app/(app)/(organization)/settings/billing/_components/current-plan.card.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { usePlan } from "@/hooks/use-plan";
 import { Subscription } from "@better-auth/stripe";
-import { Button } from "@workspace/ui/components/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@workspace/ui/components/card";
 import { Component } from "@workspace/ui/components/utils/component";
-import { isYearlyPlan, Plan } from "@workspace/ui/lib/pricing";
-import { Loader2 } from "lucide-react";
-import { useState } from "react";
+import { getBtterStatus, isYearlyPlan, Plan } from "@/lib/pricing";
+import { CancelSubscriptionDialog } from "./cancel-subscription.dialog";
+import { Badge } from "@workspace/ui/components/badge";
+import { Calendar } from "lucide-react";
+import { dayJS } from "@/lib/dayjs";
+import { cloneElement } from "react";
 
 type BillingCurrentPlanCardProps = {
   plan: Plan | undefined;
@@ -15,15 +16,9 @@ type BillingCurrentPlanCardProps = {
 };
 
 export const BillingCurrentPlanCard: Component<BillingCurrentPlanCardProps> = ({ plan, subscription }) => {
-  const [pending, setPending] = useState(false);
-
   if (!plan) return <></>;
 
-  // return (
-  //   <pre>
-  //     {JSON.stringify({ plan, subscription }, null, 2)}
-  //   </pre>
-  // )
+  const status = getBtterStatus(subscription.status);
 
   return (
     <Card>
@@ -32,33 +27,30 @@ export const BillingCurrentPlanCard: Component<BillingCurrentPlanCardProps> = ({
         <CardDescription>Manage your subscription and billing details</CardDescription>
       </CardHeader>
       <CardContent className="space-y-4">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center border border-muted rounded-md p-4">
           <div>
-            <p className="text-2xl font-semibold">{plan.name}</p>
+            <p className="text-2xl font-bold">{plan.name}</p>
             <p className="text-muted-foreground">
-              ${isYearlyPlan(subscription.priceId ?? "") ? plan.price?.yearly : plan.price?.monthly}
-              ,00 per {isYearlyPlan(subscription.priceId ?? "") ? "year" : "month"}
+              ${isYearlyPlan(subscription) ? plan.price?.yearly : plan.price?.monthly},00 per {isYearlyPlan(subscription) ? "year" : "month"}
             </p>
           </div>
 
-          <Button size={"sm"}>
-            {pending ? <Loader2 className="animate-spin" /> : "Manage Subscription"}
-          </Button>
+          <CancelSubscriptionDialog />
         </div>
 
         <div className="space-x-2 flex flex-row">
-          {/* <Badge variant={"default"} className="flex items-center gap-1.5 text-sm">
-            <CheckCircle2 size={14} />
-            <span>{status}</span>
+          <Badge variant={status.badgeVariant} className="flex items-center gap-1.5 text-sm">
+            {cloneElement(status.icon, { size: 14 })}
+            <span>{status.text}</span>
           </Badge>
 
           <Badge className="flex items-center gap-1.5 text-sm" variant={"secondary"}>
             <Calendar size={14} />
             {subscription.cancelAtPeriodEnd
               ? "Will cancel at period end"
-              : <span>Next billing date: {dayJS(subscription.currentPeriodEnd).format("MMMM D, YYYY")}</span>
+              : <span>Next billing date: {dayJS(subscription.periodEnd).format("MMMM D, YYYY")}</span>
             }
-          </Badge> */}
+          </Badge>
         </div>
       </CardContent>
     </Card>
