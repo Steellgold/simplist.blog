3baed4a73f84c00cc0083b3e5146af4d9ad2aca4
Gaëtan H <me@gaetanhus.fr>
2025-03-04 15:52:12 +0100

    feat(billing): add BillingInvoicesCard component to display invoice details

--- apps/web/app/(app)/(organization)/settings/billing/page.tsx
+++ apps/web/app/(app)/(organization)/settings/billing/page.tsx
@@ -1,20 +1,28 @@
 import { ReactElement } from "react";
 import { BreadcrumbSetter } from "@workspace/ui/components/setter-breadcrumb";
-import { auth } from "@/lib/auth";
-import { forbidden } from "next/navigation";
+import { auth, stripeClient } from "@/lib/auth";
+import { forbidden, unauthorized } from "next/navigation";
 import { headers } from "next/headers";
 import { checkPermission } from "@/lib/check-permission";
 import { BillingCurrentPlanCard } from "./_components/current-plan.card";
 import { getPlanByName, parsePlanName } from "@/lib/pricing";
+import { BillingInvoicesCard } from "./_components/invoices.card";
 
 const OrganizationBilling = async(): Promise<ReactElement> => {
-  const [organization] = await Promise.all([ auth.api.getFullOrganization({ headers: await headers() }) ]);
+  const [organization, session] = await Promise.all([
+    auth.api.getFullOrganization({ headers: await headers() }),
+    auth.api.getSession({ headers: await headers() }),
+  ]);
 
-  if (!organization) forbidden();
+  if (!session) return unauthorized();
+  if (!organization) return forbidden();
   await checkPermission({ organizationId: organization.id,permission: { settings: ["delete"] } });
 
   const subscription = (await auth.api.listActiveSubscriptions({ headers: await headers(), query: { referenceId: organization.id } }))[0]
   if (!subscription || !subscription.stripeSubscriptionId) return <div>No subscriptions found</div>;
+
+  const charges = await stripeClient.charges.list({ customer: subscription.stripeCustomerId });
+  if (!charges) return <div>No charges found</div>;
 
   return (
     <>
@@ -26,9 +34,10 @@ const OrganizationBilling = async(): Promise<ReactElement> => {
         ]
       } />
 
-      <div>
+      <>
         <BillingCurrentPlanCard plan={getPlanByName(parsePlanName(subscription.plan))} subscription={subscription} />
-      </div>
+        <BillingInvoicesCard {...charges} />
+      </>
     </>
   );
 }
