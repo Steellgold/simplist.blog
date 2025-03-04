4d933813192662d40cd8d6a792a9aac972f15bea
Gaëtan H <me@gaetanhus.fr>
2025-03-04 15:52:07 +0100

    feat(customer-portal): add CustomerPortalButton component with light variant

---
+++ apps/web/app/(app)/(organization)/settings/billing/_components/customer-portal.button.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@workspace/ui/components/button";
+
+export const CustomerPortalButton = () => {
+  return (
+    <Button size={"sm"} variant={"light"}>
+      Customer Portal
+    </Button>
+  );
+}
