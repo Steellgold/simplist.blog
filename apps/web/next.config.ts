961d0ba298fccd7d2588b545927c287cbb01d2ae
Gaëtan H <gaetanhus@gmail.com>
2025-03-07 19:12:14 +0100

    feat(config): 😎😎

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      { hostname: "cdn.simplist.blog" },
+      { hostname: "cdn.simplist.blog" }, // 😎😎😎
       { hostname: "avatars.githubusercontent.com" }
     ]
   }
