e8a4a2b26b93ec78a49609fe62454c07dc97f614
Gaëtan H <me@gaetanhus.fr>
2025-10-12 17:16:34 +0200

    feat: add initial Prisma schema with User, Session, Account, and Verification models

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { lastLoginMethod } from "better-auth/plugins";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  plugins: [
+    lastLoginMethod()
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }
+  }
+});
