59cf069d6f15a87318759dc1f1663b81c6895c35
Gaëtan H <me@gaetanhus.fr>
2025-10-16 11:28:27 +0200

    chore: update .gitignore, enhance Next.js configuration for image handling, and add AWS SDK dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true
-  }
-};
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "cdn.simplist.blog" },
+      { protocol: "https", hostname: "*.r2.cloudflarestorage.com" }
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
