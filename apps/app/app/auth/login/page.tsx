97736003b748a7fe6ba93c50875bf6d1dcc0cb34
Gaëtan H <gaetanhus@gmail.com>
2025-12-02 15:51:51 +0100

    fix(metadata): update robots meta tags for login and register pages
    - Changed the robots meta tags for both the Login and Register pages to allow indexing and following by search engines.
    - This adjustment enhances the visibility of these pages in search results, improving user access.

--- apps/app/app/auth/login/page.tsx
+++ apps/app/app/auth/login/page.tsx
@@ -4,7 +4,10 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Login",
-  robots: { index: false, follow: false }
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 const LoginPage = () => {
