50716e1ad251945adeb8473757c7a1cb171584ed
Gaëtan H <gaetanhus@gmail.com>
2025-12-14 15:52:12 +0100

    feat(ui): enhance Button component with new 'xs' size variant

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react"
 
 import { cn } from "@simplist/ui/lib/utils"
 
@@ -24,6 +24,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        xs: "h-7.5 px-2 py-1 has-[>svg]:px-2",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
@@ -61,3 +62,4 @@ function Button({
 }
 
 export { Button, buttonVariants }
+
