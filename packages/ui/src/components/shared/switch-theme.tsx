644ab15c800f77e6d6a46d7f3ad297a89a6b5d76
Gaëtan H <gaetanhus@gmail.com>
2025-12-17 23:46:56 +0100

    refactor(theme-switcher): improve ThemeSwitcher component with type safety and enhanced styling
    - Updated ThemeSwitcherProps to use a more specific Theme type for better type safety.
    - Refactored the component to include a variant prop for customizable styling options.
    - Enhanced the mounted state handling to display a Skeleton loader while the component is initializing.
    - Improved the button rendering logic and styling for active themes, ensuring a more visually appealing user experience.

--- packages/ui/src/components/shared/switch-theme.tsx
+++ packages/ui/src/components/shared/switch-theme.tsx
@@ -1,54 +1,42 @@
-'use client';
+"use client";
 
-import { useControllableState } from '@radix-ui/react-use-controllable-state';
-import { cn } from '@simplist/ui/lib/utils';
-import { Monitor, Moon, Sun } from 'lucide-react';
-import { motion } from 'motion/react';
-import { useTheme } from 'next-themes';
-import { useCallback, useEffect, useState } from 'react';
+import { useControllableState } from "@radix-ui/react-use-controllable-state";
+import { cn } from "@simplist/ui/lib/utils";
+import { Monitor, Moon, Sun } from "lucide-react";
+import { motion } from "motion/react";
+import { useTheme } from "next-themes";
+import { FC, useCallback, useEffect, useState } from "react";
+import { Skeleton } from "../skeleton";
+
+export type Theme = "light" | "dark" | "system";
 
 const themes = [
-  {
-    key: 'system',
-    icon: Monitor,
-    label: 'System theme',
-  },
-  {
-    key: 'light',
-    icon: Sun,
-    label: 'Light theme',
-  },
-  {
-    key: 'dark',
-    icon: Moon,
-    label: 'Dark theme',
-  },
+  { key: "system", icon: Monitor, label: "System theme" },
+  { key: "light", icon: Sun, label: "Light theme" },
+  { key: "dark", icon: Moon, label: "Dark theme"},
 ];
 
 export type ThemeSwitcherProps = {
-  value?: 'light' | 'dark' | 'system';
-  onChange?: (theme: 'light' | 'dark' | 'system') => void;
-  defaultValue?: 'light' | 'dark' | 'system';
+  value?: Theme;
+  onChange?: (theme: Theme) => void;
+  defaultValue?: Theme;
   className?: string;
+  variant?: "default" | "card";
 };
 
-export const ThemeSwitcher = ({
-  value,
-  onChange,
-  defaultValue,
-  className,
-}: ThemeSwitcherProps) => {
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ value, onChange, defaultValue, className, variant = "default" }) => {
   const { theme: currentTheme, setTheme: setT } = useTheme();
 
   const [theme, setTheme] = useControllableState({
-    defaultProp: defaultValue ?? (currentTheme as 'light' | 'dark' | 'system'),
+    defaultProp: defaultValue ?? (currentTheme as Theme),
     prop: value,
     onChange,
   });
 
   const [mounted, setMounted] = useState(false);
+
   const handleThemeClick = useCallback(
-    (themeKey: 'light' | 'dark' | 'system') => {
+    (themeKey: Theme) => {
       setTheme(themeKey);
       setT(themeKey);
     },
@@ -61,43 +49,59 @@ export const ThemeSwitcher = ({
   }, []);
 
   if (!mounted) {
-    return null;
+    return (
+      <div className="border rounded-full p-0.5">
+        <Skeleton className="w-20 h-8 rounded-full" />
+      </div>
+    );
   }
 
   return (
-    <div
-      className={cn(
-        'relative isolate flex h-8 rounded-full bg-background p-1 ring-1 ring-border',
-        className
-      )}
-    >
-      {themes.map(({ key, icon: Icon, label }) => {
-        const isActive = theme === key;
-        return (
-          <button
-            aria-label={label}
-            className="relative h-6 w-6 rounded-full"
-            key={key}
-            onClick={() => handleThemeClick(key as 'light' | 'dark' | 'system')}
-            type="button"
-          >
-            {isActive && (
-              <motion.div
-                className="absolute inset-0 rounded-full bg-secondary"
-                layoutId="activeTheme"
-                transition={{ type: 'spring', duration: 0.5 }}
-              />
-            )}
-
-            <Icon
-              className={cn(
-                'relative z-10 m-auto h-4 w-4',
-                isActive ? 'text-foreground' : 'text-muted-foreground'
+    <div className="border rounded-full p-0.5">
+      <div
+        className={cn(
+          "relative isolate flex h-8 rounded-full p-1 ring-1 ring-border",
+          variant === "card" && "bg-accent",
+          variant === "default" && "bg-background",
+          className
+        )}
+      >
+        {themes.map(({ key, icon: Icon, label }) => {
+          const isActive = theme === key;
+          return (
+            <button
+              aria-label={label}
+              className="relative h-6 w-6 rounded-full"
+              key={key}
+              onClick={() => handleThemeClick(key as Theme)}
+              type="button"
+            >
+              {isActive && (
+                <motion.div
+                  className={cn(
+                    "absolute inset-0 rounded-full", {
+                      "bg-primary": variant === "card" && isActive,
+                      "bg-accent": variant === "default" && isActive
+                    }
+                  )}
+                  layoutId="activeTheme"
+                  transition={{ type: "spring", duration: 0.5 }}
+                />
               )}
-            />
-          </button>
-        );
-      })}
+
+              <Icon
+                className={cn(
+                  "relative z-10 m-auto h-4 w-4", {
+                    "text-primary-foreground dark:text-white": variant === "card" && isActive,
+                    "text-muted-foreground": variant === "card" && !isActive
+                  }
+                  // isActive ? "text-primary-foreground" : "text-muted-foreground"
+                )}
+              />
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
